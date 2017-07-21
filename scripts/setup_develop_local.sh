55d2d73b49ccadf653ef24c5d62f763a8f06a33d
Klaus Reuter <khr@rzg.mpg.de>
2017-07-21 15:57:38 +0200

    updated scripts such that they can be called from anywhere

--- scripts/setup_develop_local.sh
+++ scripts/setup_develop_local.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-
-#
 # Link all packages from ~/.local to the source location, for development.
-#
-
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 ./scripts/update_git_hash.py
-
 python setup.py clean config build develop --user --cython $*
-
--- scripts/setup_install_local.sh
+++ scripts/setup_install_local.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-#
 # Link all packages from ~/.local to the source location, for development.
-#
-
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+./scripts/update_git_hash.py
 python setup.py clean config build install --user
-
