6ad84370845c18126c5240b52bbe98b977067f95
Klaus Reuter <khr@rzg.mpg.de>
2017-07-25 16:07:32 +0200

    drafted parallel filters

---
+++ capriqorn/preproc/filter/parallel.py
@@ -0,0 +1,129 @@
+"""Capriqorn parallelization filter
+
+The parallel filter is used to exploit data parallelism in pipelines.  It uses
+the Python multiprocessing module.
+
+Say we would like to parallelize a pipeline consisting of
+
+         r f f f f w
+
+i.e. a reader, four filters, and a writer.  Assume that the innermost two filters
+may trivially run in parallel.  Using the parallel filter we are able to achieve
+this as follows e.g. using five processes:
+
+         r        reader                process 0
+         f        filter
+         o        parallel fork
+------------------------------------------------------------
+    o    o    o   parallel fork         process 2, 3, 4
+    f    f    f
+    f    f    f
+    o    o    o   parallel join
+------------------------------------------------------------
+         o        parallel join         process 1
+         f        filter
+         w        writer
+
+
+The pipeline setup needs to be implemented as follows:
+
+* pipeline must be set up piecewise
+* parallel zones are defined between fork and join
+* central question is when the fork shall take place
+* multiprocessing queues must be created before the fork
+
+"""
+
+
+__author__ = "Juergen Koefinger, Klaus Reuter"
+__copyright__ = "Copyright (C) 2015-2017 Juergen Koefinger, Klaus Reuter"
+__license__ = "license_tba"
+
+
+from cadishi import base
+
+
+class Parallel(base.Filter):
+    """A filter that puts base.Container() objects into a queue and fetches
+    the results from another queue, in order to allow to parallelize the
+    pipeline of generators.
+    """
+    _depends = []
+    _conflicts = []
+
+    def __init__(self, source=-1, verbose=False, input_queue=None,
+                 output_queue=None, worker_id=-1, n_workers):
+        """ Initialize instance of parallel class.
+        worker_id is zero or positive only in parallel setups.
+        """
+        self.src = source
+        self.verb = verbose
+        if (worker_id >= 0):
+            if (input_queue is None):
+                assert(output_queue is not None)
+            if (output_queue is None):
+                assert(input_queue is not None)
+        self.input_queue = input_queue
+        self.output_queue = output_queue
+        self.worker_id = worker_id
+        self.n_workers = n_workers  # total number of processes
+        self._depends.extend(super(base.Filter, self)._depends)
+        self._conflicts.extend(super(base.Filter, self)._conflicts)
+
+    def get_meta(self):
+        """ Return information on the present filter, ready to be added to a
+        frame object's list of pipeline meta information.
+        """
+        meta = {}
+        label = 'Parallel'
+        param = {'worker_id': worker_id,
+                 'n_workers': n_workers}
+        meta[label] = param
+        return meta
+
+    def next(self):
+        if (self.worker_id < 0):
+            # do not use multiprocessing, act as a dummy generator
+            for frame in self.src.next():
+                assert isinstance(frame, base.Container)
+                frame.put_meta(self.get_meta())
+                if self.verb:
+                    print "Parallel.next() :", frame.i
+                yield frame
+        elif (self.worker_id == 0):
+            # use multiprocessing, read from a generator, put objects into queue
+            for frame in self.src.next():
+                assert isinstance(frame, base.Container)
+                frame.put_meta(self.get_meta())
+                if self.verb:
+                    print "Parallel.next() :", frame.i
+                self.input_queue.put(frame)
+            for i in range(self.n_workers-2):
+                # send None in order to communicate that the work is done
+                frame = None
+                self.input_queue.put(frame)
+        elif (self.worker_id == 1):
+            # while we read successfully from the output queue ...
+            while True:
+                frame = self.output_queue.get(frame)
+                # catch None as a signal that no more work is to be done
+                if frame is None:
+                    break
+                yield frame
+        else:
+            # task working on the central part of the pipeline:
+            # we differentiate between the opening and the closing filter
+            # simply by evaluating the presence of the queues
+            if (self.input_queue is not None):
+                while True:
+                    frame = self.input_queue.get(frame)
+                    if frame is None:
+                        break
+                    yield frame
+            elif (self.output_queue is not None):
+                for frame in self.src.next():
+                    assert isinstance(frame, base.Container)
+                    frame.put_meta(self.get_meta())
+                    self.output_queue.put(frame)
+            else:
+                pass
