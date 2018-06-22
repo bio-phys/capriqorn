df65fa9eaf3acfb743d3bb400a315267acd6af58
Klaus Reuter <khr@mpcdf.mpg.de>
2018-06-22 10:08:25 +0200

    transferred md reader from capriqorn to cadishi

--- capriqorn/preproc/io/__init__.py
+++ capriqorn/preproc/io/__init__.py
@@ -1,7 +1,7 @@
 # -*- Mode: python; tab-width: 4; indent-tabs-mode:nil; coding: utf-8 -*-
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4 fileencoding=utf-8
-# 
-# Capriqorn --- CAlculation of P(R) and I(Q) Of macRomolcules in solutioN 
+#
+# Capriqorn --- CAlculation of P(R) and I(Q) Of macRomolcules in solutioN
 #
 # Copyright (c) Juergen Koefinger, Klaus Reuter, and contributors.
 # See the file AUTHORS.rst for the full list of contributors.
@@ -11,5 +11,5 @@
 from cadishi.io.hdf5 import *
 from cadishi.io.ascii import *
 from cadishi.io.pickel import *
+from cadishi.io.md import *
 from .dummy import *
-from .md import *
